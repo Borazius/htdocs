1f2b1868ad242b689c0e66742de6c5769c3dfd92
Bora Oenal <bora@webmundo.de>
2017-04-16 13:43:28 +0200

    Übungsdateien in https-Ordner verschieben; TESTEN AB JETZT VIA WEBSERVER

--- index.php
+++ index.php
@@ -14,6 +14,19 @@
 <div>
   <?php
     echo 2 + 3;
-    echo "Hallo Bo!";
+    // <!-- Punkt bedeutet 'und' -->
+    echo "Hallo Welt" . "!";
    ?>
 </div>
+
+<?php
+  $word = "Hallo Bora";
+  echo $word;
+?>
+<br />
+<?php
+  $a = 5;
+  $b = 6;
+
+  echo $a + $b;
+?>
